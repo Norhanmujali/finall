182e6be1359236dc8a3ac9c2656f6c77b2a8e3ac
Norhanmujali <92052532+Norhanmujali@users.noreply.github.com>
2021-12-08 01:27:47 -0500

    Update testSelectionsort.java

---
+++ testSelectionsort.java
@@ -0,0 +1,54 @@
+package lab10;
+
+import org.junit.Test;
+
+public class testSelectionSort {
+@Test
+public void test() {
+testPositive();
+testNegative();
+
+}
+
+   public testSelectionSort() {
+   }
+   
+   public void testPositive(){
+       int[] arr = new int[5];
+       arr[0] = 8;
+       arr[1] = 9;
+       arr[2] = 7;
+       arr[3] = 10;
+       arr[4] = 2;
+       
+       int[] Sortedarr = new int[5];
+       Sortedarr[0] = 2;
+       Sortedarr[1] = 7;
+       Sortedarr[2] = 8;
+       Sortedarr[3] = 9;
+       Sortedarr[4] = 10;
+       
+       /** add tests to check for this unit test **/
+   }
+   
+   public void testNegative(){
+       
+       /** Test data contains negative values only **/
+   } int[] arr1 = new int[5]; {
+   arr1[0] = -8;
+   arr1[1] = -9;
+   arr1[2] = -7;
+   arr1[3] = -10;
+   arr1[4] = -2;
+   
+   int[] Sortedarr1 = new int[5];
+   Sortedarr1[0] = 2;
+   Sortedarr1[1] = 7;
+   Sortedarr1[2] = 8;
+   Sortedarr1[3] = 9;
+   Sortedarr1[4] = 10;}
+   
+   
+  
+
+
